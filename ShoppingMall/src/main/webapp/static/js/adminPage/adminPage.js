a3642d2696b96bc94d3db8122b50c289d3770f4c
sayyoup <123839647+sayyoup@users.noreply.github.com>
2023-06-20 10:41:11 +0900

    [세엽] setting

--- ShoppingMall/src/main/webapp/static/js/adminPage/adminPage.js
+++ ShoppingMall/src/main/webapp/static/js/adminPage/adminPage.js
@@ -8,12 +8,6 @@ document.addEventListener('DOMContentLoaded', function(){
 	// 개발자 화면의 드롭select.option의 value를 확인하고, 표시할 화면 선택
 
 	const menu = document.querySelector('#menu');			
-	let product = document.querySelector('div#product').innerHTML;
-	let user = document.querySelector('div#user').innerHTML;
-	let quseries = document.querySelector('div#quseries').innerHTML;
-	let review = document.querySelector('div#review').innerHTML;
-	let post = document.querySelector('div#post').innerHTML;
-	let selectedContens = document.querySelector('div#selectedContens');
 
 	menu.addEventListener('change', function(){
 		const selectedMenu = menu.value;
@@ -23,23 +17,23 @@ document.addEventListener('DOMContentLoaded', function(){
 
 			alert(`상품페이지${selectedMenu}`);
 
-			selectedContens.innerHTML = `${product}`;
+			selectedContens.innerHTML = ``;
 
 		}else if(selectedMenu=="user"){
 
-			selectedContens.innerHTML = `${user}`;
+			selectedContens.innerHTML = ``;
 
 		}else if(selectedMenu=="quseries"){
 
-			selectedContens.innerHTML = `${quseries}`;
+			selectedContens.innerHTML = ``;
 
 		}else if(selectedMenu=="review"){
 
-			selectedContens.innerHTML = `${review}`;
+			selectedContens.innerHTML = `}`;
 
 		}else if(selectedMenu=="post"){
 
-			selectedContens.innerHTML = `${post}`;
+			selectedContens.innerHTML = ``;
 		}
 
 	});
